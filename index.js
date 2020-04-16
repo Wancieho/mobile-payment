70b276071d3a3185f7bd819468184f5bce71c461
Wancieho <wancieho@gmail.com>
2020-04-17 00:40:58 +0200

    tweaks

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var https = require('https');
-var querystring = require('querystring');
+const https = require('https');
+const querystring = require('querystring');
 
 function request(callback) {
-  var path = '/v1/checkouts';
-  var data = querystring.stringify({
+  const path = '/v1/checkouts';
+  const data = querystring.stringify({
     'entityId': '8a8294174e735d0c014e78cf26461790',
     'amount': '92.00',
     'currency': 'ZAR',
     'paymentType': 'DB'
   });
-  var options = {
+
+  const options = {
     port: 443,
     host: 'https://test.oppwa.com',
-    path: path,
+    path,
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -20,17 +21,29 @@ function request(callback) {
       'Authorization': 'Bearer OGE4Mjk0MTc0ZTczNWQwYzAxNGU3OGNmMjY2YjE3OTR8cXl5ZkhDTjgzZQ=='
     }
   };
-  var postRequest = https.request(options, function (res) {
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      const jsonRes = JSON.parse(chunk);
-      return callback(jsonRes);
+
+  try {
+    const postRequest = https.request(options, res => {
+      res.setEncoding('utf8');
+
+      res.on('data', chunk => {
+        const jsonRes = JSON.parse(chunk);
+
+        return callback(jsonRes);
+      });
+
+      res.on('error', error => {
+        console.debug(error);
+      });
     });
-  });
-  postRequest.write(data);
-  postRequest.end();
+
+    postRequest.write(data);
+    postRequest.end();
+  } catch (error) {
+    console.debug(error);
+  }
 }
 
-request(function (responseData) {
+setTimeout(() => request((responseData) => {
   console.log(responseData);
-});
+}), 2000);
